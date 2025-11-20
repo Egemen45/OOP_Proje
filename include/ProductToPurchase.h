794fe4945eb0aa81c72809eb57c2b0c70cd809d3
Egemen45 <egemenozer5@gmail.com>
2025-11-20 16:07:06 +0300

    egemen updated

--- include/ProductToPurchase.h
+++ include/ProductToPurchase.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "product.h"
 class ProductToPurchase
 {
+private : 
+	Product* product;
+	int quantity;
+
+public : 
+	Product* getProduct();
+	void setProduct(Product* product);
+	int getQuantity();
+	void setQuantity(int quantity);
+
 };
 
--- include/ShoppingCart.h
+++ include/ShoppingCart.h
@@ -1,5 +1,28 @@
 #pragma once
+#include "ProductToPurchase.h"
+#include "Payment.h"
+#include "Customer.h"
+#include <list>
+
 class ShoppingCart
 {
+private : 
+	std::list<ProductToPurchase*> productsToPurchase;
+	Payment* paymentMethod;
+	Customer* customer;
+	bool isBonusUsed;
+
+public : 
+	Payment* getPaymentMethod();
+	void setPaymentMethod(Payment* paymentMethod);
+	Customer* getCustomer();
+	void setCustomer(Customer* customer);
+	void setBonusUsed();
+	void addProduct(Product* product);
+	void removeProduct(Product product);
+	void placeOrder();
+	void cancelOrder();
+	void printProducts();
+	void showInvoice();
 };
 

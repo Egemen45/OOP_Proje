f0f97e0f812dcdaf55ca2baa5ea725c3d5b42b6e
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:47:22 +0300

    ilk hal

--- src/Magazine.cpp
+++ src/Magazine.cpp
@@ -1,1 +1,30 @@
 #include "Magazine.h"
+using namespace std;
+#include <iostream>
+
+
+Magazine::Magazine(int _issue,std::string _type):issue(_issue),type(_type)
+{
+}
+void Magazine:: printProperties()const {
+
+	std::cout<<"Issue: "<<issue<<std::endl;
+	std::cout << "Type: " << type << std::endl;
+}
+
+int Magazine::getIssue() const {
+	return issue;
+
+}
+std::string Magazine::getType() const {
+	return type;
+}
+void  Magazine::setIssue(int issue) {
+	this->issue = issue;
+	
+}
+
+void  Magazine::setType(std::string type) {
+	this->type = type;
+
+}

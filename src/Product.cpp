e9aa7697f8854b1b427feb93411c063e92e08d43
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:48:11 +0300

    Implement getters and setters for Product classilk hali

--- src/Product.cpp
+++ src/Product.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
 #include "Product.h"
+using namespace std;
+
+int Product ::getID() const{
+	return ID;
+
+
+}
+void Product::setID(int ID) {
+	this->ID = ID;
+
+
+}
+std::string Product::getName()const {
+	
+	return name;
+
+
+}
+void Product:: setName(std::string name) {
+	this->name = name;
+
+
+}
+double Product::getPrice()const {
+
+	return price;
+
+
+}
+void Product:: setPeice(double price) {
+	this->price = price;
+
+
+}
+void Product::printProperties() {
+	cout << "ID: " << ID << endl
+		<< "name: " << name << endl
+		<< "price: " << price << endl;
+
+
+
+
+}
+
+
+
+
+

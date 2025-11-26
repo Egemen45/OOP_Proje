15c914bca8065ae6f6edde394e1ff0e89e7aec66
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:44:23 +0300

    ilkel

--- include/Book.h
+++ include/Book.h
@@ -1,5 +1,43 @@
-#pragma once
-class Book
-{
+//polymorphisim uygulanmamış hali!!!
+#ifndef BOOK_H
+#define BOOK_H
+
+#include <iostream>
+#include<string>
+
+#include "Product.h"//üst sınıfın tanımlı olduğu dosyayı ekliyoruz
+
+
+
+class Book : public Product { //inheritage uygulamasını gerçekliştiriyoruz
+private:
+	std::string author;       //private değişkenlerimizin tanımını yaptık
+	std::string publisher;
+	int page;
+public:
+	//fonksiyon tanımlamaları 
+	void printProperties();                  //bilgileri yazdıran fonksiyon
+
+	std::string getAuthor()const;              //yazar ismini döndüren fonksiyon   
+	void setAuthor(std::string author);        //yazar ismini değiştiren fonksiyon
+
+	std::string getPublisher() const;            //yayın ismini döndüren fonksiyon
+	void setPublisher(std::string publisher);   //yayın ismini değiştiren fonksiyon
+
+	int getPage()const;                       //sayfa saısını döndüren fonksiyon
+	void setPage(int page);                   //sayfa saısını yeni sayfa ile güncelleyen  fonksiyon
+	
+
+
+
 };
+
+
+
+
+
+
+
+#endif
+
 

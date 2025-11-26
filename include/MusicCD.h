6362e196a7d0a3335bc968c70f0f0fb041421dac
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:45:20 +0300

    ilkel

--- include/MusicCD.h
+++ include/MusicCD.h
@@ -1,5 +1,24 @@
-#pragma once
-class MusicCD
-{
+//polymorphism uygulanmadı
+#ifndef MUSICCD_H
+#define MUSICCD_H
+
+#include<iostream>
+#include "Product.h"
+
+class MusicCD:public Product {
+private:
+	std::string singer;
+	std::string type;
+public:
+              
+	void printProperties()const;             //constu ben ekledim
+	std::string getSinger()const;         //constu ben ekledim
+	void getSinger(std::string singer);
+	std::string getType()const;          //constu ben ekledim
+	void setType(std::string type);
+
+
 };
 
+
+#endif

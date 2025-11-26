a3c867372d4f4da81ff3ddf1f895ab8e4512e202
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:46:54 +0300

    ilk hal

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,1 +1,40 @@
+
 #include "Book.h"
+#include <iostream>
+
+
+void Book::printProperties() {
+	std::cout << "Author: " << author << std::endl
+		      << "Publisher: " << publisher << std::endl
+		      << "Page: " << page << std::endl;
+
+
+}
+std::string Book::getAuthor() const{
+
+	return author;
+}
+
+void Book:: setAuthor(std::string author) {
+	this->author = author;
+
+}
+std::string Book::getPublisher()const{
+
+	return publisher;
+}
+void Book::setPublisher(std::string publisher) {
+	
+	this->publisher = publisher;
+
+}
+int Book::getPage() const{
+	return page;
+
+
+}
+void Book::setPage(int page) {
+	this->page = page;
+	
+}
+

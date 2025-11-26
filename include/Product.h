2e5f451007cd8414d51ef4740f2b22788765f17b
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:46:14 +0300

    soyut degil
    Added member variables and methods for Product class.

--- include/Product.h
+++ include/Product.h
@@ -1,5 +1,26 @@
-#pragma once
-class Product
-{
-};
+//soyut sınıf kısmı yok
 
+#ifndef PRODUCT_H
+#define PRODUCT_H
+
+#include<iostream>
+
+class Product{
+private:
+	int ID;
+	std::string name;
+	double price;
+public:
+	int getID()const;
+	void setID(int ID);
+	std::string getName()const;
+	void setName(std::string name);
+	double getPrice()const;
+	void setPeice(double price);
+	void printProperties();
+
+
+
+
+};
+#endif

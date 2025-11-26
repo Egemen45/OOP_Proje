5944d42b0603d8a05bf5d0793f9f396d2fda42ed
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-26 22:47:48 +0300

    ilk hal

--- src/MusicCD.cpp
+++ src/MusicCD.cpp
@@ -1,1 +1,30 @@
 #include "MusicCD.h"
+#include <iostream>
+
+std::string MusicCD::getSinger()const {
+
+	return singer;
+}
+
+void MusicCD:: getSinger(std::string singer) {
+
+	this->singer = singer;
+
+}
+
+std::string MusicCD::getType() const{
+
+	return type;
+
+}
+
+void MusicCD::setType(std::string type){
+	this->type = type;
+
+}
+void MusicCD::printProperties() const{
+	std::cout << "Singer: " << singer << std::endl;
+	std::cout << "Type: " << type << std::endl;	
+
+
+}

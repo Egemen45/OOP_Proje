4dd85381235a9ae418e5ea9e6a56bba2b4ccd822
Hava-ISIKLI <hava.isikli13@gmail.com>
2025-11-30 18:05:07 +0300

    Refactor Customer class header and add methods

--- include/Customer.h
+++ include/Customer.h
@@ -1,5 +1,56 @@
 #pragma once
-class Customer
-{
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+class Customer{
+private:
+    long customerID;
+    string name;
+    string address;
+    string phone;
+    double bonus;
+    string email;
+    string username;
+    string password;
+public:
+    Customer();
+    Customer(long id, const string& n, const string& u, const string& p);
+    long getCustomerID()const;
+    void setCustomerID(long customerID);
+
+    string getName()const;
+    void setName(const string& name);
+
+    string getAddress()const;
+    void setAddress(const string& address);
+
+    string getPhone()const;
+    void setPhone(const string& phone);
+
+    double getBonus()const;
+    void setBonus(double bonus);
+
+    string getEmail()const;
+    void setEmail(const string& email);
+
+    string getUsername()const;
+    void setUsername(const string& username);
+
+    string getPassword()const;
+    void setPassword(const string& password);
+
+    // Diðer Metotlar (Functionalities)
+    void sendBill();
+    bool checkAccount(const std::string& uname, const std::string& pass) const;
+    void addBonus(double bonusAmount);
+    void useBonus();
+    void printProperties() const;
+    ~Customer();
+
 };
+
+#endif // CUSTOMER_H
 

344cc2ec42a6c416749232cc3e8e68e2b401033a
haticesenbaba <senbabahatice53@gmail.com>
2025-11-30 22:40:26 +0300

    Add header guards and improve Payment class
    Refactor Payment class with proper header guards and comments.

--- include/Payment.h
+++ include/Payment.h
@@ -1,5 +1,26 @@
 #pragma once
-class Payment
-{
+#ifndef PAYMENT_H
+#define PAYMENT_H
+
+
+class Payment {
+private:
+    double amount; // odeme tutari
+
+public:
+    // constructor 
+    Payment(double amount = 0.0);
+
+    // destructor
+    virtual ~Payment() = default;       //bellek sizitisini önlemek icin sanal yikici kullandik
+
+    double getAmount() const;           //kapsulleme ile veri okuma 
+
+    void setAmount(double amount);      //kapsulleme ile vei yazma disardan private degiskene erisim
+  
+    virtual void performPayment() const = 0;   //dinamik baglanma icin saf sanal baglanma
+                                               //alt siniflarda da otomatik olarak gerceklestirilecek
 };
+
+#endif 
 

962f24fe85b972de80846f8887c271e0daf76f6f
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-30 21:22:54 +0300

    son
    Added a pure virtual function and updated method signatures.

--- include/Product.h
+++ include/Product.h
@@ -1,23 +1,30 @@
-//soyut sınıf kısmı yok
+//BUSE KONYALI
+//30.11.2025
+
 
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
 #include<iostream>
 
-class Product{
+class Product{ //sınıf tanımı
 private:
 	int ID;
 	std::string name;
 	double price;
 public:
+	virtual void printProperties() const= 0;    //soyut sınıf için en az bir tane sanal fonksiyon olmalı 
+
+	Product(int _ID = 0, std::string _name = "", double _price = 0.0); //constructor ile değerleri varsayılan olarak başlatıyoruz
+
+	//fonksiyon prototipleri tanıtılıyor
 	int getID()const;
 	void setID(int ID);
-	std::string getName()const;
+	std::string getName()const; //sadece değer okuyanlar ,nesnenin değerini değiştirmeyenler const olarak tanımlanıyor
 	void setName(std::string name);
 	double getPrice()const;
-	void setPeice(double price);
-	void printProperties();
+	void setPrice(double price);
+	
 
 
 

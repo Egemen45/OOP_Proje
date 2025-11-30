af6ce753d1d9c8c7dca8ea3a3a3c704a7de3bf91
haticesenbaba <senbabahatice53@gmail.com>
2025-11-30 22:42:46 +0300

    Add include guards to Cash.h and enhance comments
    Added include guards and improved class documentation.

--- include/Cash.h
+++ include/Cash.h
@@ -1,5 +1,21 @@
 #pragma once
-class Cash
-{
+#ifndef CASH_H
+#define CASH_H
+
+#include "Payment.h"
+
+
+class Cash : public Payment {     //cash sinifi payment sinifindan türetilmistir
+public:
+    // constructor
+    Cash(double amount = 0.0);
+
+    // destructor
+    virtual ~Cash() = default;    //bellek sizintisini önemek ve asamali yikim icin
+                                  //sanal yikici
+
+    void performPayment() const override;  //sanal metodu gerceklestirir
 };
+
+#endif 
 

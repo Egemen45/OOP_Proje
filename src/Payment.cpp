ea17dd7584ec1d3addb7230ad4cf45e8dc2db1c9
haticesenbaba <senbabahatice53@gmail.com>
2025-11-30 22:41:39 +0300

    Add Payment class with constructor and accessors
    Implement Payment class constructor and methods

--- src/Payment.cpp
+++ src/Payment.cpp
@@ -1,1 +1,17 @@
 #include "Payment.h"
+
+//gercek odemeler payment sınıfında degil ondan türeyecek olan 
+// alt siniflarda gerceklestiriecek cunku payment soyut bir siniftir
+
+// constructor 
+Payment::Payment(double amount) : amount(amount) {}
+
+double Payment::getAmount() const {     // amount degerini dondurur
+    return amount;
+}
+
+
+void Payment::setAmount(double amount) {   //amount degeri girme
+    this->amount = amount;
+}
+

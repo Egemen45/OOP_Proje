b86c5647bf833503e6c73dbeca735a63e4fa1c96
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-30 21:26:34 +0300

    son
    Refactor Book class constructor and printProperties method to improve clarity and functionality.

--- src/Book.cpp
+++ src/Book.cpp
@@ -1,15 +1,30 @@
+//BUSE KONYALI
+//30.11.2025
 
-#include "Book.h"
+
+#include "Book.h"  //.h dosyasını tanımlıyoruz
 #include <iostream>
+using namespace std;
 
 
-void Book::printProperties() {
-	std::cout << "Author: " << author << std::endl
+//constructor ile değerler başlatılıyor
+Book::Book(int _ID , std::string _name , double _price ,std::string _author , std::string _publisher, int _page ):Product(_ID,_name,_price),author(_author),publisher(_publisher),page(_page){}
+
+
+
+void Book::printProperties()const {
+
+	cout << "ID: " << getID() << endl;                   //product sınıfının bilgilerini yazdırıyoruz 
+	cout << "Name: " << getName() << endl;               //public inheritance sayesinde Book içinde erişim sağladık
+	cout << "Price: " << getPrice() << endl;                  
+
+	std::cout << "Author: " << author << std::endl       //Book sınıfının bilgilerini yazdırıyoruz
 		      << "Publisher: " << publisher << std::endl
 		      << "Page: " << page << std::endl;
 
 
 }
+//fonksiyonların  görevleri yazılıyor
 std::string Book::getAuthor() const{
 
 	return author;
@@ -37,4 +52,5 @@ void Book::setPage(int page) {
 	this->page = page;
 	
 }
+
 

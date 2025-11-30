aebaa9751f36b75cc661c39b4463574a8fe21782
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-11-30 21:25:35 +0300

    son

--- src/MusicCD.cpp
+++ src/MusicCD.cpp
@@ -1,12 +1,20 @@
+//BUSE KONYALI
+//30.11.2025
+
 #include "MusicCD.h"
 #include <iostream>
+using namespace std;
 
+//constructor ile ddeğerler başlatılıyor
+
+MusicCD::MusicCD(int _ID , std::string _name , double _price, std::string _singer , std::string _type):Product(_ID, _name, _price), singer(_singer), type(_type){}
+//fonksiyonların içleri dolduruluyor
 std::string MusicCD::getSinger()const {
 
 	return singer;
 }
 
-void MusicCD:: getSinger(std::string singer) {
+void MusicCD:: setSinger(std::string singer) {
 
 	this->singer = singer;
 
@@ -22,7 +30,14 @@ void MusicCD::setType(std::string type){
 	this->type = type;
 
 }
+//bilgilerin yazdırılması için gerekli fonksiyon
 void MusicCD::printProperties() const{
+
+	cout << "ID: " << getID() << endl;
+	cout << "Name: " << getName() << endl;
+	cout << "Price: " << getPrice() << endl;
+
+
 	std::cout << "Singer: " << singer << std::endl;
 	std::cout << "Type: " << type << std::endl;	
 

27877cf37dc7390d3b679fb438ecbc77348c5771
haticesenbaba <senbabahatice53@gmail.com>
2025-11-30 22:44:33 +0300

    Add performPayment method to Cash class
    Implement cash payment functionality in Cash class.

--- src/Cash.cpp
+++ src/Cash.cpp
@@ -1,1 +1,14 @@
 #include "Cash.h"
+#include <iostream>
+
+using namespace std;
+
+// Miras alınan Payment yapıcısını çağırır
+Cash::Cash(double amount) : Payment(amount) {}
+
+// Nakit ödeme yapar
+void Cash::performPayment() const {
+    
+    cout << " Nakit odeme islemi basariyla gerceklestirildi" <<endl;
+    cout << " Odenen Tutar: " << getAmount() <<endl;
+}

b154626ac76b2457237d03cc596c9fc32fe4f9be
pelinece <pegercel@gmail.com>
2025-12-03 18:52:57 +0300

    .

--- include/Check.h
+++ include/Check.h
@@ -1,5 +1,25 @@
-#pragma once
-class Check
-{
-};
+#ifndef Check_h
+#define Check_h
+//Pelin Ece Gerçel 01.12.2025
+//ders notlarindan yararlanilmistir.
+#include <string>
+#include <iostream>
+using namespace std;
+#include "Payment.h" //include the Payment.h file for generalization (inheritance) relationship
+class Check : public Payment { //parent: Payment, child: Check
+								//Check should inherit performPayment() method from Payment
 
+private: //attributes and methods according to the UML diagram
+	string name; //name of the check owner
+	string bankID; //bank ID of the check
+
+public:
+	Check(string name = "a", string bankID = "b"); //constructor with default parameters
+	~Check(); //destructor
+	void performPayment(); //method from Payment class
+	string getName(); //get the name of the check owner
+	void setName(string); //set the name of the check owner
+	string getBankID(); //get the bank ID of the check
+	void setBankID(string); //set the bank ID of the check
+};
+#endif
--- include/CreditCard.h
+++ include/CreditCard.h
@@ -1,5 +1,28 @@
-#pragma once
-class CreditCard
-{
-};
+#ifndef CreditCard_h
+#define CreditCard_h
+//Pelin Ece Gerçel 01.12.2025
+//ders notlarindan yararlanilmistir.
+#include <string>
+#include <iostream>
+using namespace std;
+#include "Payment.h" //include the Payment.h file for generalization (inheritance) relationship
+class CreditCard : public Payment { //parent: Payment, child: Credit Card
+									//Credit Card should inherit performPayment() method from Payment
 
+private: //attributes and methods according to the UML diagram
+	long number; //credit card number
+	string type; //type of the credit card (Visa, MasterCard)
+	string expDate; //expiration date of the credit card
+
+public:
+	CreditCard(long number = 0, string type = "x", string expDate = "3000"); //constructor with default parameters
+	~CreditCard(); //destructor
+	void performPayment(); //method from Payment class
+	long getNumber(); //get the credit card number
+	void setNumber(long); //set the credit card number
+	string getType(); //get the type of the credit card
+	void setType(string); //set the type of the credit card
+	string getExpDate(); //get the expiration date of the credit card
+	void setExpDate(string); //set the expiration date of the credit card
+};
+#endif

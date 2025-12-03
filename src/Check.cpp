5df73a6dc1f1b1b4d452d99d6ac7238628e3be9a
pelinece <pegercel@gmail.com>
2025-12-03 18:54:01 +0300

    .

--- src/Check.cpp
+++ src/Check.cpp
@@ -1,1 +1,23 @@
-#include "../include/Check.h"
+#include "Check.h"
+Check::Check(string n, string id) { //if there are parameters assign them to name, bankID,
+									//if not use default values
+	name = n;
+	bankID = id;
+}
+Check::~Check() {
+}
+void Check::performPayment() { //how does the payment occur with check details
+	cout << "payment with check from: " << name << endl;
+}
+string Check::getName() { //return the name of the check owner
+	return name;
+}
+void Check::setName(string n) { //set the name (n) to the check's name
+	name = n;
+}
+string Check::getBankID() { //return the bank ID of the check
+	return bankID;
+}
+void Check::setBankID(string id) { //set the bank ID (id) to the check's bankID
+	bankID = id;
+}
--- src/CreditCard.cpp
+++ src/CreditCard.cpp
@@ -1,1 +1,30 @@
-#include "../include/CreditCard.h"
+#include "CreditCard.h"
+CreditCard::CreditCard(long n, string t, string d) { //if there are parameters assign them to name, type, expDate,
+													//if not use default values
+	number = n;
+	type = t;
+	expDate = d;
+}
+CreditCard::~CreditCard() {
+}
+void CreditCard::performPayment() { //how does the payment occur with giving credit card details
+	cout << "payment with credit card number: " << number << " type: " << type << endl;
+}
+long CreditCard::getNumber() { //return the credit card number
+	return number;
+}
+void CreditCard::setNumber(long n) { //set the number (n) to the credit card's number
+	number = n;
+}
+string CreditCard::getType() { //return the type of the credit card
+	return type;
+}
+void CreditCard::setType(string t) { //set the type (t) to the credit card's type
+	type = t;
+}
+string CreditCard::getExpDate() { //return the expiration date of the credit card
+	return expDate;
+}
+void CreditCard::setExpDate(string d) { //set the expiration date (d) to the credit card's expDate
+	expDate = d;
+}

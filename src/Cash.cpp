82b07f6ce6471cc1eab7e077795bc51b42f9122a
haticesenbaba <senbabahatice53@gmail.com>
2025-12-07 12:17:33 +0300

    hata kontrolleri

--- src/Cash.cpp
+++ src/Cash.cpp
@@ -1,16 +1,24 @@
 //HATİCE ŞENBABA
+//01.12.2025
 
-#include "../include/Cash.h"
+#include "Cash.h"
 #include <iostream>
+#include <stdexcept>
+
 
 using namespace std;
 
-// Miras alınan Payment yapıcısını çağırır
-Cash::Cash(double amount) : Payment(amount) {};
+// constructor: Miras alınan Payment yapıcısını çağırır 
+Cash::Cash(double amount) : Payment(amount) {
+    if (amount < 0) {                             //hata kontrolu
+        throw std::invalid_argument("Odeme tutari negatif olamaz.");
+    }
+}
 
 // Nakit ödeme yapar
 void Cash::performPayment() const {
-    
-    cout << " Nakit odeme islemi basariyla gerceklestirildi" <<endl;
-    cout << " Odenen Tutar: " << getAmount() <<endl;
+    cout << "Nakit odeme islemi basariyla gerceklestirildi" << endl;
+    cout << "Odenen Tutar: " << getAmount() << endl;
 }
+
+

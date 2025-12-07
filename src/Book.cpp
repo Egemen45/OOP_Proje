26d21e82c7887594c7bef1798f99f598e92b75e9
Buse-01000010 01110101 01110011 01100101 <b14busekonyali@gmail.com>
2025-12-07 18:36:21 +0300

    düzeltme

--- src/Book.cpp
+++ src/Book.cpp
@@ -31,14 +31,13 @@ Book::Book(int _ID , std::string _name , double _price ,std::string _author , st
 }
 void Book::printProperties()const {
 
-	std::cout << "ID: " << getID() << std::endl;                   //product sınıfının bilgilerini yazdırıyoruz
-	std::cout << "Name: " << getName() << std::endl;               //public inheritance sayesinde Book içinde erişim sağladık
-	std::cout << "Price: " << getPrice() << std::endl;
+	std::cout << "        ID: " << getID() << std::endl;                   //product sınıfının bilgilerini yazdırıyoruz
+	std::cout << "        Name: " << getName() << std::endl;               //public inheritance sayesinde Book içinde erişim sağladık
+	std::cout << "        Price: " << getPrice() << std::endl;
 
-	std::cout << "Author: " << author << std::endl              //Book sınıfının bilgilerini yazdırıyoruz
-		      << "Publisher: " << publisher << std::endl
-		      << "Page: " << page << std::endl;
-
+	std::cout << "        Author: " << author << std::endl              //Book sınıfının bilgilerini yazdırıyoruz
+		      << "        Publisher: " << publisher << std::endl
+		      << "        Page: " << page << std::endl;
 
 }
 //fonksiyonların  görevleri yazılıyor

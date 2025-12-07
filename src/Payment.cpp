607fb4b6aaceecd060583c51830bb6f5e723fd5a
haticesenbaba <senbabahatice53@gmail.com>
2025-12-07 12:15:14 +0300

    hata kontrolleri

--- src/Payment.cpp
+++ src/Payment.cpp
@@ -1,19 +1,32 @@
 //HATİCE ŞENBABA
+//01.12.2025
 
-#include "../include/Payment.h"
-
-//gercek odemeler payment sınıfında degil ondan türeyecek olan 
+#include "Payment.h"
+#include <stdexcept>
+//gercek odemeler payment sınıfında degil ondan türeyecek olan
 // alt siniflarda gerceklestiriecek cunku payment soyut bir siniftir
 
-// constructor 
-Payment::Payment(double amount) : amount(amount) {}
+// constructor
+Payment::Payment(double amount)
+{
+    if (amount < 0) {
+        throw std::invalid_argument("Odeme tutari negatif olamaz.");
+    }
 
-double Payment::getAmount() const {     // amount degerini dondurur
+    this->amount = amount;
+}
+
+double Payment::getAmount() const {    // amount degerini dondurur
     return amount;
 }
 
+void Payment::setAmount(double amount) {      //amount degeri girme
 
-void Payment::setAmount(double amount) {   //amount degeri girme
+    if (amount < 0) {
+        throw std::invalid_argument("Odeme tutari negatif olamaz.");
+    }
+
     this->amount = amount;
 }
+
 

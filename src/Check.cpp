680979c7e196952c5ad39be6237c17573698e569
Hava-ISIKLI <hava.isikli13@gmail.com>
2025-12-09 00:08:21 +0300

    HATA KONTROLERİ TAMAM

--- src/Check.cpp
+++ src/Check.cpp
@@ -1,4 +1,4 @@
-#include "../include/Check.h"
+/*#include "../include/Check.h"
 Check::Check(string n, string id) { //if there are parameters assign them to name, bankID,
 									//if not use default values
 	name = n;
@@ -21,4 +21,56 @@ string Check::getBankID() { //return the bank ID of the check
 void Check::setBankID(string id) { //set the bank ID (id) to the check's bankID
 	bankID = id;
 }
+*/
 
+//HAVA IŞIKLI 
+//08.12.2025 HATA KONTROLLERI EKLENDI
+#include "Check.h"
+Check::Check(string n, string id) { //if there are parameters assign them to name, bankID,
+									//if not use default values
+	if(n.empty()){
+        cout<<"isim bulunamadi 'Unknown' atamasi yapildi.\n"<<endl;
+        n="Unknown";
+	}
+	name = n;
+	if(id.empty()){
+        cout<<"bankID bulunamadi '0000' atamasi yapildi.\n"<<endl;
+        id="0000";
+	}
+	bankID = id;
+}
+Check::~Check() {
+}
+void Check::performPayment() const { //how does the payment occur with check details
+	cout << "payment with check from: " << name << " , bank: "<< bankID << " , amount: " << getAmount()<< endl;
+}
+string Check::getName() { //return the name of the check owner
+	return name;
+}
+void Check::setName(string n) { //set the name (n) to the check's name
+    //bos alan kontrolu
+    if(n.empty()){
+        cout<<"name bos birakilamaz!!\n"<<endl;
+        return;
+    }
+    //karakter bosluk ya da harf degilse hata mesaji
+    for(char c: n){
+        if(!isalpha(c)&& !isspace(c)){
+            cout<<"hatali karakter girdiniz!!\n"<<endl;
+            return;
+        }
+    }
+    //hata kontrolleri gectiyse atama yap
+	name = n;
+}
+string Check::getBankID() { //return the bank ID of the check
+	return bankID;
+}
+void Check::setBankID(string id) { //set the bank ID (id) to the check's bankID
+    if (id.empty()) {
+        cout << "BankID bos olamaz!!\n"<<endl;
+        return;
+    }
+    //bos degilse atama yapalim
+	bankID = id;
+}

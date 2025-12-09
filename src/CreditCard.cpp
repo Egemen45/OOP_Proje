e84a9f11ead55a7f2b054188702980b157f5579a
Hava-ISIKLI <hava.isikli13@gmail.com>
2025-12-09 16:31:21 +0300

    HATA KONTROLÜ YAPTIM AMA BAŞKA HATALARI D KONTROL ETMEM GEREKİYOR MU SİZCE
    Added validation checks for credit card number, type, and expiration date in setters.

--- src/CreditCard.cpp
+++ src/CreditCard.cpp
@@ -1,4 +1,4 @@
-#include "../include/CreditCard.h"
+#include "CreditCard.h"
 CreditCard::CreditCard(long n, string t, string d) { //if there are parameters assign them to name, type, expDate,
 													//if not use default values
 	number = n;
@@ -13,19 +13,37 @@ void CreditCard::performPayment() const { //how does the payment occur with givi
 long CreditCard::getNumber() { //return the credit card number
 	return number;
 }
+//hata kontrolleri tamam
 void CreditCard::setNumber(long n) { //set the number (n) to the credit card's number
+    if(n<0){
+        cout<<"kart numarasi negatif olamaz!!\n\n"<<endl;
+        return;
+    }
+
+    //kontrolden gectiyse atama yap
 	number = n;
 }
 string CreditCard::getType() { //return the type of the credit card
 	return type;
 }
 void CreditCard::setType(string t) { //set the type (t) to the credit card's type
+    if(t.empty()){
+        cout<<"kart turu bos olamaz !!\n\n"<<endl;
+        return;
+    }
+    //bos degilse atama yap
 	type = t;
 }
 string CreditCard::getExpDate() { //return the expiration date of the credit card
 	return expDate;
 }
 void CreditCard::setExpDate(string d) { //set the expiration date (d) to the credit card's expDate
+    if(d.empty()){
+        cout<<"ExpDate bos birakilamaz \n\n"<<endl;
+        return;
+    }
+    //kontrol yapildiysa atama yap
 	expDate = d;
 
 }
+
